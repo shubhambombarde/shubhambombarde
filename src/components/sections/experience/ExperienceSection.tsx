04399ffb1bd5fdffd90318a20c15ff7492674daa
Shubham Bombarde <shubhambombarde4@gmail.com>
2024-01-21 21:24:37 +0530

    added experience section

--- src/components/sections/experience/ExperienceSection.tsx
+++ src/components/sections/experience/ExperienceSection.tsx
@@ -1,132 +1,90 @@
-export default function ExperienceSection() {
+import "./ExperienceSection.scss";
+
+function ExperienceItem(payload: {
+  timeline: string;
+  companyName: string;
+  jobTitle: string;
+  jobDescription: string[];
+  skillsList: string[];
+}) {
   return (
-    <section id="projects">
+    <article className="row">
+      <div className="experience-wrapper__left-column">
+        <p className="experience-wrapper__text-info">{payload.timeline}</p>
+      </div>
+
+      <div className="experience-wrapper__text">
+        <h3 className="experience-wrapper__text-title">
+          {payload.companyName}
+        </h3>
+        <h1 className="experience-wrapper__text-subtitle">
+          {payload.jobTitle}
+        </h1>
+        <ul className="experience-wrapper__text-info">
+          {payload.jobDescription.map((item) => (
+            <li key={item}>
+              <p>{item}</p>
+            </li>
+          ))}
+        </ul>
+        <div className="experience-wrapper__text-btns">
+          {payload.skillsList.map((item) => (
+            <p key={item} className="skills">
+              {item}
+            </p>
+          ))}
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default function ExperienceSection() {
+  const data = [
+    {
+      timeline: "APRIL 2022 - PRESENT",
+      companyName: "Kickoff by Zuju",
+      jobTitle: "Software Developer",
+      jobDescription: [
+        "Developing robust, user-friendly front-end features for both the user application and back-office systems, enhancing the user experience and operational efficiency.",
+        "Working closely with UX/UI designers to translate design wireframes into responsive and interactive web applications.",
+        "Collaborating and communicating with cross-functional teams, including backend developers and designers, to ensure seamless integration and consistency across the platform.",
+        "Dedicated to continuous learning, I stay abreast of the latest frontend trends and technologies, including Next JS features and the seamless integration of GitHub Copilot into workflow processes. Proactively making recommendations for tech stack enhancements.\n",
+      ],
+      skillsList: ["React JS", "NextJS", "Typescript", "Cypress", "GraphQL"],
+    },
+    {
+      timeline: "MARCH 2021 - MARCH 2022",
+      companyName: "TOKIGAMES",
+      jobTitle: "Frontend Developer",
+      jobDescription: [
+        "Developed robust and efficient front-end code for the company's platform, ensuring a seamless user experience. ",
+        "Implemented and optimized the user app, enhancing its performance and ensuring its alignment with business objectives. ",
+        "Proactively identified and resolved bottlenecks and bugs, employing strategic problem-solving techniques to minimize system downtime and enhance overall performance. ",
+        "Contributed to the creation and upkeep of documentation, assisting in onboarding new team members and serving as a knowledge base for ongoing development.",
+      ],
+      skillsList: ["React JS", "NextJS", "Typescript", "Cypress", "WebSockets"],
+    },
+    {
+      timeline: "JULY 2019 - FEBRUARY 2021",
+      companyName: "SEARCE",
+      jobTitle: "Software Developer",
+      jobDescription: [
+        "Developed and maintained a robust platform for a movie production company, facilitating real-time tracking of progress, financials, and pre-production planning. ",
+        "Demonstrated proficiency in both frontend and backend development, ensuring efficient, testable, and scalable code. ",
+        "Collaborated closely with an overseas client, providing them with vital development assistance, and ensuring their project met stipulated benchmarks and quality standards.",
+      ],
+      skillsList: ["Angular", "React JS", "Mongo DB", "Python"],
+    },
+  ];
+  return (
+    <section id="experience">
       <div className="container">
-        <div className="project-wrapper">
+        <div className="experience-wrapper">
           <h2 className="section-title dark-blue-text">Experience</h2>
-
-          {/*// <!-- Each .row is a project -->*/}
-          <article className="row">
-            <div className="project-wrapper__text">
-              <h3 className="project-wrapper__text-title">Project Title</h3>
-              <p className="project-wrapper__text-info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deleniti voluptas, sit nam perspiciatis tempore aspernatur ullam
-                necessitatibus impedit repellendus vero sed ipsa eos beatae ab
-                sint saepe nostrum! Enim, perferendis.
-              </p>
-              <div className="project-wrapper__text-btns">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="cta-btn cta-btn--hero cta-btn--projects"
-                >
-                  See Live
-                </a>
-                <a href="#" target="_blank" className="cta-btn text-color-main">
-                  Source Code
-                </a>
-              </div>
-            </div>
-
-            <div className="project-wrapper__image">
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <div className="thumbnail rounded">
-                  <img
-                    src="/assets/project.png"
-                    className="img-fluid"
-                    alt="Project Image"
-                    width="1366"
-                    height="767"
-                  />
-                </div>
-              </a>
-            </div>
-          </article>
-          {/*// <!-- /END Project -->*/}
-
-          {/*// <!-- Each .row is a project -->*/}
-          <article className="row">
-            <div className="project-wrapper__text">
-              <h3 className="project-wrapper__text-title">Project Title</h3>
-              <p className="project-wrapper__text-info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deleniti voluptas, sit nam perspiciatis tempore aspernatur ullam
-                necessitatibus impedit repellendus vero sed ipsa eos beatae ab
-                sint saepe nostrum! Enim, perferendis.
-              </p>
-              <div className="project-wrapper__text-btns">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="cta-btn cta-btn--hero cta-btn--projects"
-                >
-                  See Live
-                </a>
-                <a href="#" target="_blank" className="cta-btn text-color-main">
-                  Source Code
-                </a>
-              </div>
-            </div>
-
-            <div className="project-wrapper__image">
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <div className="thumbnail rounded">
-                  <img
-                    src="/assets/project.png"
-                    className="img-fluid"
-                    alt="Project Image"
-                    width="1366"
-                    height="767"
-                  />
-                </div>
-              </a>
-            </div>
-          </article>
-          {/*// <!-- /END Project -->*/}
-
-          {/*// <!-- Each .row is a project -->*/}
-          <article className="row">
-            <div className="project-wrapper__text">
-              <h3 className="project-wrapper__text-title">Project Title</h3>
-              <p className="project-wrapper__text-info">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deleniti voluptas, sit nam perspiciatis tempore aspernatur ullam
-                necessitatibus impedit repellendus vero sed ipsa eos beatae ab
-                sint saepe nostrum! Enim, perferendis.
-              </p>
-              <div className="project-wrapper__text-btns">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="cta-btn cta-btn--hero cta-btn--projects"
-                >
-                  See Live
-                </a>
-                <a href="#" target="_blank" className="cta-btn text-color-main">
-                  Source Code
-                </a>
-              </div>
-            </div>
-
-            <div className="project-wrapper__image">
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <div className="thumbnail rounded">
-                  <img
-                    src="/assets/project.png"
-                    className="img-fluid"
-                    alt="Project Image"
-                    width="1366"
-                    height="767"
-                  />
-                </div>
-              </a>
-            </div>
-          </article>
-          {/*// <!-- /END Project -->*/}
+          {data.map((item) => (
+            <ExperienceItem key={item.timeline} {...item} />
+          ))}
         </div>
       </div>
     </section>
