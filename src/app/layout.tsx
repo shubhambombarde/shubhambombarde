a1a5b0d2130998f54ea1598937ce729659aebfdc
Shubham Bombarde <shubhambombarde4@gmail.com>
2024-01-29 18:25:24 +0530

    added SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { metadata as _metadata } from "@/constants/metadata";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
