612b2018d3dc9aeee917ae67c2673b8e6d5b06c4
Shubham Bombarde <shubhambombarde4@gmail.com>
2024-02-01 04:42:53 +0530

    updated sitemap and metadata

--- src/constants/metadata.tsx
+++ src/constants/metadata.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Shubham Bombarde | Developer",
-  description: "React, Next JS, TypeScript, GraphQL, Artificial Intelligence",
+  title: "Shubham Bombarde | Software Developer",
+  description:
+    "I build pixel-perfect, accessible products for the web and beyond - [React, Next JS, TypeScript, Python, Artificial Intelligence]",
+  keywords: ["Frontend Developer", "React JS", "Next JS", "AI"],
 };
